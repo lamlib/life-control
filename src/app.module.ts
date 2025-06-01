112d9f56e14073e35f16fc7b67aa4b19c3007438
lamlib <lamlib2023@gmail.com>
2025-05-31 22:52:40 -0700

    feat: config file env and import config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { TypeOrmModule, TypeOrmModuleAsyncOptions, TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'; 
 import { UserAccount } from './users/userAccount.entity';
 import { UserPermission } from './users/userPermission.entity';
 import { UserRole } from './users/userRole.entity';
@@ -13,21 +13,29 @@ import { HashingAlgorithm } from './users/hashingAlgorithm.entity';
 import { EmailValidationStatus } from './users/emailValidationStatus.entity';
 import { UserLoginExternal } from './users/userLoginExternal.entity';
 import { ExternalProvider } from './users/externalProvider.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [ `.env.${process.env.NODE_ENV}`, `.env.example` ]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<string>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [UserAccount, UserPermission, UserRole, UserRolePermission, UserLogin, HashingAlgorithm, EmailValidationStatus, UserLoginExternal, ExternalProvider],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'Lamlibok@123',
-      database: 'clover',
-      entities: [UserAccount, UserPermission, UserRole, UserRolePermission, UserLogin, HashingAlgorithm, EmailValidationStatus, UserLoginExternal, ExternalProvider],
-      synchronize: true,
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
