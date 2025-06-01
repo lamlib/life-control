0ab764b9b466ffdfdc8a54882ad93cd18c9b8109
lamlib <lamlib2023@gmail.com>
2025-06-01 09:59:50 -0700

    doc: intergrate swagger

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserPermission } from 'src/users/userPermission.entity';
+import { UserRole } from 'src/users/userRole.entity';
+import { UserRolePermission } from 'src/users/userRolePermission.entity';
 
 @Module({
   imports: [
     UsersModule,
+    TypeOrmModule.forFeature([UserPermission, UserRole, UserRolePermission])
   ],
   providers: [ AuthService ],
   controllers: [AuthController],
-  exports: [AuthService]
+  exports: [ AuthService ]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // https://docs.nestjs.com/openapi/introduction
+  const config = new DocumentBuilder()
+  .setTitle('Clover application')
+  .setDescription('Life control API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
